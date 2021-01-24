1eb59a0061d571badde9e78706925f15c23d83fa
shivam <shivam@shivams-MBP.domain.name>
2021-01-24 17:25:19 +0530

    added media queries to make it responsive

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ let landingPage = document.querySelector(".landing-page");
 let ProjectsPage1 = document.querySelector(".project-page1");
 let ProjectsPage2 = document.querySelector(".project-page2");
 let contactPage = document.querySelector(".contact-page");
+let button = document.querySelector("#button");
+let svg = document.querySelector("#svg");
 let mouseHoverclass;
+let linkcolor;
 console.log(landingPage);
 const Cursor = (e) => {
   mouseCursor.style.top = e.pageY + "px";
@@ -18,7 +21,7 @@ window.addEventListener("mousemove", Cursor);
 navLinks.forEach((link) => {
   link.addEventListener("mouseover", () => {
     mouseCursor.classList.add(mouseHoverclass);
-    link.classList.add("hovered-link");
+    link.style.color = linkcolor;
     smallCursor.style.display = "none";
   });
   link.addEventListener("mouseleave", () => {
@@ -31,6 +34,7 @@ landingPage.addEventListener("mouseover", () => {
   mouseCursor.style.borderColor = "var(--pink)";
   smallCursor.style.borderColor = "var(--cream)";
   mouseHoverclass = "link-grow";
+  linkcolor = "var(--blue)";
   navLinks.forEach((link) => {
     link.style.color = "var(--cream)";
   });
@@ -39,6 +43,7 @@ aboutPage.addEventListener("mouseover", () => {
   mouseCursor.style.borderColor = "var(--cream)";
   smallCursor.style.borderColor = "var(--blue)";
   mouseHoverclass = "cursor-bg";
+  linkcolor = "var(--pink)";
   navLinks.forEach((link) => {
     link.style.color = "var(--blue)";
   });
@@ -47,6 +52,7 @@ ProjectsPage1.addEventListener("mouseover", () => {
   mouseCursor.style.borderColor = "var(--pink)";
   smallCursor.style.borderColor = "var(--cream)";
   mouseHoverclass = "link-grow";
+  linkcolor = "var(--blue)";
   navLinks.forEach((link) => {
     link.style.color = "var(--cream)";
   });
@@ -55,15 +61,24 @@ ProjectsPage2.addEventListener("mouseover", () => {
   mouseCursor.style.borderColor = "var(--pink)";
   smallCursor.style.borderColor = "var(--cream)";
   mouseHoverclass = "link-grow";
+  linkcolor = "var(--blue)";
   navLinks.forEach((link) => {
     link.style.color = "var(--cream)";
   });
 });
 contactPage.addEventListener("mouseover", () => {
-  mouseCursor.style.borderColor = "var(--pink)";
-  smallCursor.style.borderColor = "var(--blue)";
+  mouseCursor.style.borderColor = "var(--blue)";
+  smallCursor.style.borderColor = "var(--pink)";
   mouseHoverclass = "grow";
+  linkcolor = "var(--cream)";
   navLinks.forEach((link) => {
     link.style.color = "var(--blue)";
   });
+});
+
+button.addEventListener("mouseover", () => {
+  svg.style.fill = "rgb(51, 250, 250)";
+});
+button.addEventListener("mouseleave", () => {
+  svg.style.fill = "rgb(18, 32, 32)";
 });
